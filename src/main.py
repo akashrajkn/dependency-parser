3ce65c40626e28f80f025548a15f91439f714cad
akashrajkn <akashrajkn@gmail.com>
2017-11-20 17:22:32 +0100

    added preprocessing in pseudocode

--- src/main.py
+++ src/main.py
@@ -1,17 +1,34 @@
+def preprocess():
+	pass
+
+
+def word_embedding(corpus):
+
+	return word
+
+
 
 def dependency_parser():
+
+	word_embeddings = word_embedding(corpus)
+	pos_embeddings = word_embedding(corpus_pos_tags)
+
+    # initialize U_1
 
     for each sentence:
         x = sentence
         for each word:
-            word_embedding = get_word_embeddings
-            pos_embedding = get_pos_embeddings()
+            # word_embedding = get_word_embeddings
+            word_embedding = word_embeddings[word]
+
+            # pos_embedding = get_pos_embeddings()
+            pos_embedding = word_embedding[pos]
             x[i] = concatenate()
-        
+
         r = LSTM_forward(x) + LSTM_backward(x)
 
+        # score
         h_head = MLP_head(r)
         h_dep = MLP_dep(r)
 
-        # score
-        s[i] = h_head.T * U_1 * h_dep[i] + h_head.T * u_2 
+        s[i] = h_head.T * U_1 * h_dep[i] + h_head.T * u_2
