fd2a39e69d0a67d3a760b3e82bd10e8e66e113d5
Jack-Harding1 <31994187+Jack-Harding1@users.noreply.github.com>
2017-12-12 18:51:05 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def convert_sentence_to_adjancency_matrix(sentence):
+def convert_sentence_to_adjacency_matrix(sentence):
     '''
     Input: sentence in json
-    Output: adjancency matrix (gold standard)
+    Output: adjacency matrix (gold standard)
     '''
 
     sentence_len = len(sentence['words'])
 
     # Initialize a matrix of size N x N
-    adjancency_matrix = np.zeros((sentence_len, sentence_len))
+    adjacency_matrix = np.zeros((sentence_len, sentence_len))
 
     for word in sentence['words']:
         word_id = int(word['id'])
@@ -20,9 +20,23 @@ def convert_sentence_to_adjancency_matrix(sentence):
         if head == -1:
             continue
 
-        adjancency_matrix[head][word_id] = 1
+        adjacency_matrix[head][word_id] = 1
 
-    return adjancency_matrix
+    return adjacency_matrix
 
-def convert_adjancecy_matrix_to_sentece(matrix):
-    pass
+def adjacency_matrix_to_tensor(matrix):
+    output = [0] * matrix.shape[0]
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[0]):
+            if matrix[i][j] == 1:
+                output[j] = i
+    output1 = torch.LongTensor(output)
+    return(output1)
+
+test_matrix = np.array(([1, 0, 1, 1, 0],
+                       [0, 0, 0, 0, 0],
+                       [0, 1, 0, 0, 0],
+                       [0, 0, 0, 0, 1],
+                       [0, 0, 0, 0, 0]))
+print(test_matrix)
+print(adjacency_matrix_to_tensor(test_matrix))
