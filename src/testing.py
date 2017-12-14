4a3f2bcda57c2568b392bd7013e8526e4ba989fd
Jack-Harding1 <31994187+Jack-Harding1@users.noreply.github.com>
2017-12-14 17:32:50 +0100

    Update testing.py

--- src/testing.py
+++ src/testing.py
@@ -5,16 +5,28 @@ Created on Wed Dec 13 17:39:46 2017
 
 @author: jackharding
 """
+import numpy as np
 import torch
+import json
+import os
+
+current_path = os.path.dirname(__file__)
+filepath = current_path + '/../data/toy_data.json'
+#if filepath is None:
+#filepath = current_path + '/../data/en-ud-train.json'
+training_data = json.load(open(filepath, 'r'))
+w2i, p2i, pwe, ppe = pretrain_word_embeddings(training_data, len_word_embed, len_pos_embed)
+network = Network(w2i, p2i, pwe, ppe, len_word_embed, len_pos_embed)
 
 #takes in two tensors representing dependency trees
 #and returns their unlabelled attachement score
 def UAS_score(pred_tensor, gold_tensor):
+    length = len(pred_tensor)
     count = 0
-    for i in range(len(pred_tensor)):
+    for i in range(length):
         if pred_tensor[i] == gold_tensor[i]:
             count += 1
-    return count
+    return (count/length)*100
 
 #test_tensor = torch.LongTensor([0, 0, 1, 2, 3, 4])
 #gold_tensor = torch.LongTensor([0, 2, 1, 4, 5, 4])
@@ -30,5 +42,20 @@ def edges_to_tensor(edges):
     output1 = torch.LongTensor(output)
     return output1
 
-#test_edges = [(0, 2), (3, 5), (2, 3), (3, 4), (2, 1)]
-#print(edges_to_tensor(test_edges))
+def test(sentence):
+    gold_mat = convert_sentence_to_adjacency_matrix(sentence)
+    gold_tree = adjacency_matrix_to_tensor(gold_mat)
+    target = Variable(gold_tree, requires_grad=False)
+        
+    sequence = torch.LongTensor(len(data[i]['words']), 2)
+    for i, word in enumerate(data[i]['words']):
+        sequence[i,0] = w2i[word['form']]
+        sequence[i,1] = p2i[word['xpostag']]
+    sequence_var = Variable(sequence)
+        
+    adj_mat = network(sequence_var)
+    pred_edges = get_edmonds(adj_mat, 0)
+    pred_tensor = edges_to_tensor(pred_edges)
+        
+    score_for_sentence = UAS_score(pred_tensor, gold_tensor)
+    return score_for_sentence
